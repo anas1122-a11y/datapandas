cac099f51bc5443d69827aa7d700fbe04e33de38
RIdwan Ahmed Anas <ridwanahmedanas4@gmail.com>
2024-12-21 22:32:22 +0600

    Add files via upload

---
+++ dataframe using pandas.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+exam_data = {'name': ['anastasia', 'dima', 'katherine', 'james', 'michale', 'matyhew', 'laura', 'janas'],
+             'score': [12.5, 9, 16.5, np.nm]}
